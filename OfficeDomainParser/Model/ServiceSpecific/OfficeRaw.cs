d4c85288f2c8f0b2f5a3719159d14f249bd92c28
generalsle1n <niels.schuler@web.de>
2024-04-01 16:19:07 +0200

    Added ModelBinding for Office

---
+++ OfficeDomainParser/Model/ServiceSpecific/OfficeRaw.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OfficeDomainParser;
+public class SingleOfficeRaw
+{
+    [JsonPropertyName("id")]
+    public int ID { get; set; }
+    [JsonPropertyName("serviceArea")]
+    public string Service { get; set; }
+    [JsonPropertyName("serviceAreaDisplayName")]
+    public string DisplayName { get; set; }
+    [JsonPropertyName("urls")]
+    public List<string> Urls { get; set; }
+    [JsonPropertyName("ips")]
+    public List<string> IPs { get; set; }
+    [JsonPropertyName("tcpPorts")]
+    public string TCPPorts { get; set; }
+    [JsonPropertyName("udpPorts")]
+    public string UDPPorts { get; set; }
+    [JsonPropertyName("expressRoute")]
+    public bool ExpressRoute { get; set; }
+    [JsonPropertyName("category")]
+    public string Category { get; set; }
+    [JsonPropertyName("required")]
+    public bool Required { get; set; }
+}
