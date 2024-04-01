6405d2e30c9928da1b7557ac52a80e0369f065c9
generalsle1n <niels.schuler@web.de>
2024-04-01 16:18:41 +0200

    Added Host Service

--- OfficeDomainParser/Program.cs
+++ OfficeDomainParser/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OfficeDomainParser;
+using Serilog;
+
+const string SettingsFileName = "settings.json";
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .MinimumLevel.Debug()
+    .CreateLogger();
+
+IConfigurationRoot Config = new ConfigurationBuilder()
+    .AddJsonFile(SettingsFileName, false, true)
+    .Build();
+
+var Builder = new HostBuilder()
+    .UseSerilog()
+    .UseSystemd()
+    .UseWindowsService()
+    .ConfigureAppConfiguration((config) =>
+    {
+        config.AddJsonFile(SettingsFileName, false, true);
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<OfficeScraper>();
+        services.AddHostedService<DataScraper>();
+        services.AddDbContext<ServiceContext>(option =>
+        {
+            option.UseSqlite($"Data Source={Config.GetValue<string>("DatabaseName")}");
+
+        });
+    });
+
+IHost Host = Builder.Build();
+await Host.RunAsync();
